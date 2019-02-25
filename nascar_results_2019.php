1e962f18e3717fec11543b6366bd2efaf83cd6d7
Nick Shurtleff <nshurtleff@acumensolutions.com>
2019-02-25 10:32:42 -0500

    Added 2019 Atlanta race results

--- nascar_results_2019.php
+++ nascar_results_2019.php
@@ -3,9 +3,11 @@
 $k = 0;
 while ($k < 10) {  
     get_results($week_1_teams[$k], "results/2019_daytona_results.xml");
+    get_results($week_2_teams[$k], "results/2019_atlanta_results.xml");
     $k++;
 }
 
 get_season_points($season_drivers, "results/2019_daytona_results.xml");
+get_season_points($season_drivers, "results/2019_atlanta_results.xml");
 
 ?>
