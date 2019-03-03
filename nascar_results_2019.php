01ce982ddd3a739ffe98c94cc297063b40adbdc4
Nick Shurtleff <nshurtleff@acumensolutions.com>
2019-03-03 18:53:07 -0500

    Added 2019 las vegas results

--- nascar_results_2019.php
+++ nascar_results_2019.php
@@ -4,10 +4,12 @@ $k = 0;
 while ($k < 10) {  
     get_results($week_1_teams[$k], "results/2019_daytona_results.xml");
     get_results($week_2_teams[$k], "results/2019_atlanta_results.xml");
+    get_results($week_2_teams[$k], "results/2019_las_vegas_results.xml");
     $k++;
 }
 
 get_season_points($season_drivers, "results/2019_daytona_results.xml");
 get_season_points($season_drivers, "results/2019_atlanta_results.xml");
+get_season_points($season_drivers, "results/2019_las_vegas_results.xml");
 
 ?>
