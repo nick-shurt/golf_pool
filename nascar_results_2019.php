e764ea9acbf50da1e78fbb9d1997228194d562e2
Nick Shurtleff <nshurtleff@acumensolutions.com>
2019-03-03 19:02:16 -0500

    Fixed week of teams error in results file

--- nascar_results_2019.php
+++ nascar_results_2019.php
@@ -4,7 +4,7 @@ $k = 0;
 while ($k < 10) {  
     get_results($week_1_teams[$k], "results/2019_daytona_results.xml");
     get_results($week_2_teams[$k], "results/2019_atlanta_results.xml");
-    get_results($week_2_teams[$k], "results/2019_las_vegas_results.xml");
+    get_results($week_3_teams[$k], "results/2019_las_vegas_results.xml");
     $k++;
 }
 
