aea74023f05181bd1286060c0b6e1694e881d8fd
Nick Shurtleff <nshurtleff@acumensolutions.com>
2019-03-10 19:15:46 -0400

    Added get results and get season points for phoenix race

--- nascar_results_2019.php
+++ nascar_results_2019.php
@@ -5,12 +5,14 @@ while ($k < 10) {
     get_results($week_1_teams[$k], "results/2019_daytona_results.xml");
     get_results($week_2_teams[$k], "results/2019_atlanta_results.xml");
     get_results($week_3_teams[$k], "results/2019_las_vegas_results.xml");
+    get_results($week_4_teams[$k], "results/2019_phoenix_results.xml");
     $k++;
 }
 
 get_season_points($season_drivers, "results/2019_daytona_results.xml");
 get_season_points($season_drivers, "results/2019_atlanta_results.xml");
 get_season_points($season_drivers, "results/2019_las_vegas_results.xml");
+get_season_points($season_drivers, "results/2019_phoenix_results.xml");
 
 
 ?>
